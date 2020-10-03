5155ee301585717686c776c89b5f5d7b77b5d036
Josh Guempel <joshguempel5@gmail.com>
2020-10-03 14:56:52 -0500

    Add gin middleware

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module HackMan
 
 go 1.15
 
-require github.com/googollee/go-socket.io v1.4.4
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/googollee/go-socket.io v1.4.4
+)
--- server/server.go
+++ server/server.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 
 	socketio "github.com/googollee/go-socket.io"
 )
 
-func main() {
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		panic(err)
+func ginMiddleware(allowOrigin string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Request.Header.Del("Origin")
+
+		c.Next()
 	}
+}
+
+func main() {
+	router := gin.New()
+
+	server, _ := socketio.NewServer(nil)
 
 	server.OnConnect("/", func(s socketio.Conn) error {
 		fmt.Println("connected:", s.ID())
@@ -35,9 +53,10 @@ func main() {
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("../asset")))
+	router.Use(ginMiddleware("http://localhost:8080"))
+	router.GET("/socket.io/*any", gin.WrapH(server))
+	router.POST("/socket.io/*any", gin.WrapH(server))
+	router.StaticFS("/public", http.Dir("../asset"))
 
-	log.Println("Serving at localhost:8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router.Run()
 }
