2a105b62aad270f832057ffd53d032c7c3e84854
Tom Dong <contact@tomdong.io>
2020-10-02 23:04:57 -0500

    Added example socket.io connection

--- client/sockets.js
+++ client/sockets.js
@@ -1,4 +1,5 @@
-const socket = window.io('ws://localhost:8080');
+/*
+const socket = window.io('ws://localhost:8080/echo`');
 
 socket.on("connect", () => {
   socket.send('Hello from the client!');
@@ -12,3 +13,21 @@ socket.on("event", () => {
 socket.on("disconnect", () => {
   console.log("Socket disconnected");
 });
+*/
+
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+var socket = new WebSocket("ws://localhost:8080/echo");
+
+socket.onopen = function () {
+    output.innerHTML += "Status: Connected\n";
+};
+
+socket.onmessage = function (e) {
+    output.innerHTML += "Server: " + e.data + "\n";
+};
+
+function send() {
+    socket.send(input.value);
+    input.value = "";
+}
