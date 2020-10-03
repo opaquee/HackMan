7f5db2c0225e22e78d7a3d33fa90e8cfab4da68d
Henri Evjen <henrievjen@Henris-MacBook-Pro.local>
2020-10-03 14:50:29 -0500

    added ghost collision

--- client/index.js
+++ client/index.js
@@ -17,6 +17,10 @@ const createMap = () => {
             const el = document.createElement("div");
             el.classList.add("map-element");
     
+            if (map[i][j] === id) {
+                x = i;
+                y = j;
+            }
             if (map[i][j] == 1) {
                 el.classList.add('wall');
             }
@@ -27,11 +31,14 @@ const createMap = () => {
                 pacman.setAttribute("height", "100%");
                 pacman.classList.add("sprite");
                 el.append(pacman);
-
-                if (map[i][j] === id) {
-                    x = i;
-                    y = j;
-                }
+            }
+            else if (map[i][j] >= 200) {
+                const ghost = document.createElement("IMG");
+                ghost.setAttribute("src", "./assets/sprites/pink_ghost.png");
+                ghost.setAttribute("width", "100%");
+                ghost.setAttribute("height", "100%");
+                ghost.classList.add("sprite");
+                el.append(ghost);
             }
             row.append(el);
         }
@@ -52,7 +59,7 @@ const main = () => {
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 200, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, id, 1, 1, 1, 1, 0, 1, 0, 1],
@@ -72,44 +79,80 @@ document.addEventListener("keydown", event => {
     event.preventDefault();
     // Left
     if (event.keyCode === 37) {
-        if (x - 1 >= 0 && map[x-1][y] !== 1) {
+        if (x - 1 >= 0 && map[x-1][y] == 0) {
             deleteMap();
             map[x][y] = 0;
             x--;
-            map[x][y] = 100;
+            map[x][y] = id;
+            createMap();
+        }
+        else if (x - 1 >= 0 && map[x-1][y] >= 200 && id < 200) {
+            deleteMap();
+            map[x][y] = 0;
+            id = 201;
+            x = 7;
+            y = 7;
+            map[x][y] = id;
             createMap();
         }
     }
 
     // Up
     else if (event.keyCode === 38) {
-        if (y >= 0 && map[x][y-1] !== 1) {
+        if (y >= 0 && map[x][y-1] == 0) {
             deleteMap();
             map[x][y] = 0;
             y--;
-            map[x][y] = 100;
+            map[x][y] = id;
+            createMap();
+        }
+        else if (y >= 0 && map[x][y-1] >= 200 && id < 200) {
+            deleteMap();
+            map[x][y] = 0;
+            id = 201;
+            x = 7;
+            y = 7;
+            map[x][y] = id;
             createMap();
         }
     }
 
     // Right
     else if (event.keyCode === 39) {
-        if (x < map[x].length && map[x+1][y] !== 1) {
+        if (x < map[x].length && map[x+1][y] == 0) {
             deleteMap();
             map[x][y] = 0;
             x++;
-            map[x][y] = 100;
+            map[x][y] = id;
+            createMap();
+        }
+        else if (x < map[x].length && map[x+1][y] >= 200 && id < 200) {
+            deleteMap();
+            map[x][y] = 0;
+            id = 201;
+            x = 7;
+            y = 7;
+            map[x][y] = id;
             createMap();
         }
     }
 
     // Down
     else if (event.keyCode === 40) {
-        if (y < map.length && map[x][y+1] !== 1) {
+        if (y < map.length && map[x][y+1] == 0) {
             deleteMap();
             map[x][y] = 0;
             y++;
-            map[x][y] = 100;
+            map[x][y] = id;
+            createMap();
+        }
+        else if (y < map[x].length && map[x][y+1] >= 200 && id < 200) {
+            deleteMap();
+            map[x][y] = 0;
+            id = 201;
+            x = 7;
+            y = 7;
+            map[x][y] = id;
             createMap();
         }
     }
