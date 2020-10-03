2762ca6acf0e8c0141c219a763a1d5ef3310fa4d
Henri Evjen <henrievjen@Henris-MacBook-Pro.local>
2020-10-03 15:26:18 -0500

    updated data formats

--- client/index.js
+++ client/index.js
@@ -2,13 +2,27 @@ let map;
 let id;
 let x;
 let y;
-let ghosts = 1;
+
+let players = [
+    {
+        id: 100,
+        isGhost: false
+    },
+    {
+        id: 101,
+        isGhost: true
+    }
+];
 
 const removeAllChildNodes = (parent) => {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
-}
+};
+
+const getPlayer = (id) => {
+    return players.find(x => x.id == id);
+};
 
 const createMap = () => {
     for (let i = 0; i < map.length; i++) {
@@ -25,21 +39,23 @@ const createMap = () => {
             if (map[i][j] == 1) {
                 el.classList.add('wall');
             }
-            else if (map[i][j] >= 100 && map[i][j] < 200) {
-                const pacman = document.createElement("IMG");
-                pacman.setAttribute("src", "./assets/sprites/pacman.png");
-                pacman.setAttribute("width", "100%");
-                pacman.setAttribute("height", "100%");
-                pacman.classList.add("sprite");
-                el.append(pacman);
-            }
-            else if (map[i][j] >= 200) {
-                const ghost = document.createElement("IMG");
-                ghost.setAttribute("src", "./assets/sprites/pink_ghost.png");
-                ghost.setAttribute("width", "100%");
-                ghost.setAttribute("height", "100%");
-                ghost.classList.add("sprite");
-                el.append(ghost);
+            else if (map[i][j] >= 100) {
+                if (getPlayer(map[i][j]).isGhost) {
+                    const ghost = document.createElement("IMG");
+                    ghost.setAttribute("src", "./assets/sprites/pink_ghost.png");
+                    ghost.setAttribute("width", "100%");
+                    ghost.setAttribute("height", "100%");
+                    ghost.classList.add("sprite");
+                    el.append(ghost);
+                }
+                else {
+                    const pacman = document.createElement("IMG");
+                    pacman.setAttribute("src", "./assets/sprites/pacman.png");
+                    pacman.setAttribute("width", "100%");
+                    pacman.setAttribute("height", "100%");
+                    pacman.classList.add("sprite");
+                    el.append(pacman);
+                }
             }
             row.append(el);
         }
@@ -60,14 +76,14 @@ const main = () => {
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 200, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, id, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1, 1, 101, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
@@ -80,6 +96,7 @@ document.addEventListener("keydown", event => {
     event.preventDefault();
     // Left
     if (event.keyCode === 37) {
+        // Open Move
         if (x - 1 >= 0 && map[x-1][y] == 0) {
             deleteMap();
             map[x][y] = 0;
@@ -87,10 +104,11 @@ document.addEventListener("keydown", event => {
             map[x][y] = id;
             createMap();
         }
-        else if (x - 1 >= 0 && map[x-1][y] >= 200 && id < 200) {
+        // Move into ghost
+        else if (x - 1 >= 0 && getPlayer(map[x-1][y]).isGhost && getPlayer(id).isGhost == false) {
             deleteMap();
             map[x][y] = 0;
-            id = 200 + ghosts++;
+            getPlayer(id).isGhost = true;
             x = 7;
             y = 7;
             map[x][y] = id;
@@ -100,6 +118,7 @@ document.addEventListener("keydown", event => {
 
     // Up
     else if (event.keyCode === 38) {
+        // Open Move
         if (y >= 0 && map[x][y-1] == 0) {
             deleteMap();
             map[x][y] = 0;
@@ -107,10 +126,11 @@ document.addEventListener("keydown", event => {
             map[x][y] = id;
             createMap();
         }
-        else if (y >= 0 && map[x][y-1] >= 200 && id < 200) {
+        // Move into ghost
+        else if (y >= 0 && getPlayer(map[x][y-1]).isGhost && getPlayer(id).isGhost == false) {
             deleteMap();
             map[x][y] = 0;
-            id = 200 + ghosts++;
+            getPlayer(id).isGhost = true;
             x = 7;
             y = 7;
             map[x][y] = id;
@@ -120,6 +140,7 @@ document.addEventListener("keydown", event => {
 
     // Right
     else if (event.keyCode === 39) {
+        // Open Move
         if (x < map[x].length && map[x+1][y] == 0) {
             deleteMap();
             map[x][y] = 0;
@@ -127,10 +148,11 @@ document.addEventListener("keydown", event => {
             map[x][y] = id;
             createMap();
         }
-        else if (x < map[x].length && map[x+1][y] >= 200 && id < 200) {
+        // Move into ghost
+        else if (x < map[x].length && getPlayer(map[x+1][y]).isGhost && getPlayer(id).isGhost == false) {
             deleteMap();
             map[x][y] = 0;
-            id = 200 + ghosts++;
+            getPlayer(id).isGhost = true;
             x = 7;
             y = 7;
             map[x][y] = id;
@@ -140,6 +162,7 @@ document.addEventListener("keydown", event => {
 
     // Down
     else if (event.keyCode === 40) {
+        // Open Move
         if (y < map.length && map[x][y+1] == 0) {
             deleteMap();
             map[x][y] = 0;
@@ -147,10 +170,11 @@ document.addEventListener("keydown", event => {
             map[x][y] = id;
             createMap();
         }
-        else if (y < map[x].length && map[x][y+1] >= 200 && id < 200) {
+        // Move into ghost
+        else if (y < map[x].length && getPlayer(map[x][y+1]).isGhost && getPlayer(id).isGhost == false) {
             deleteMap();
             map[x][y] = 0;
-            id = 200 + ghosts++;
+            getPlayer(id).isGhost = true;
             x = 7;
             y = 7;
             map[x][y] = id;
